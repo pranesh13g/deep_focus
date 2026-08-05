a3b09d7b3563236e45a703df8443360dd0ec033a
Pranesh <praneshck7@gmail.com>
2026-08-06 00:38:11 +0600

    feat:firebase analythics firebase crashlythics added

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,33 @@
+import 'dart:async';
+
 import 'package:deep_focus/core/services/notification_service.dart';
 import 'package:deep_focus/core/theme/app_theme.dart';
 import 'package:deep_focus/navigation.dart';
 import 'package:deep_focus/providers.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  NotificationService.init(); // Non-blocking init
-  await ScreenUtil.ensureScreenSize();
-  runApp(
-    MultiProvider(providers: AppProviders.allProviders, child: const MyApp()),
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  // Pass all uncaught Flutter framework errors to Crashlytics
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+
+  // Catch uncaught async errors (e.g. in Futures, Streams outside Flutter)
+  await runZonedGuarded(() async {
+    NotificationService.init(); // Non-blocking init
+    await ScreenUtil.ensureScreenSize();
+    runApp(
+      MultiProvider(providers: AppProviders.allProviders, child: const MyApp()),
+    );
+  }, FirebaseCrashlytics.instance.recordError);
 }
 
 class MyApp extends StatelessWidget {
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,13 +6,21 @@ import FlutterMacOS
 import Foundation
 
 import audio_session
+import firebase_analytics
+import firebase_core
+import firebase_crashlytics
 import flutter_local_notifications
 import just_audio
+import shared_preferences_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
+  FirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FirebaseAnalyticsPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
